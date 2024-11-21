7d4b16d47088f4da54a86b667bb5c815b3fc65f1
Paul Walker <paul@pwjw.com>
2024-11-21 18:08:05 -0500

    Github Actions and Surgeify-Infra commit one
    - Clang Format
    - Add an installer target to CMakeLists which does nothing other than
      build all
    - add a build-pr and code checks action
    - make it build

--- src/LatticeComponent.h
+++ src/LatticeComponent.h
@@ -1,11 +1,11 @@
 /*
   Lattices - A Just-Intonation graphical MTS-ESP Source
-  
+
   Copyright 2023-2024 Andreya Ek Frisk and Paul Walker.
-  
+
   This code is released under the MIT licence, but do note that it depends
   on the JUCE library, see licence for more details.
-  
+
   Source available at https://github.com/Andreya-Autumn/lattices
 */
 
@@ -13,7 +13,7 @@
 
 #include "JIMath.h"
 #include "LatticesBinary.h"
-#include "LatticesAssets.h"
+// #include "LatticesAssets.h"
 
 #include <melatonin_blur/melatonin_blur.h>
 
@@ -27,7 +27,7 @@ struct LatticeComponent : juce::Component
             CoO[i] = c[i];
         }
     }
-    
+
     void update(std::pair<int, int> *c)
     {
         for (int i = 0; i < 12; ++i)
@@ -35,20 +35,20 @@ struct LatticeComponent : juce::Component
             CoO[i] = c[i];
         }
     }
-    
+
     void paint(juce::Graphics &g) override
     {
         float ctrDistance{JIRadius * (5.f / 3.f)};
-        
+
         float vDistance = 2.0f * ctrDistance;
         float hDistance = 2.0f * ctrDistance;
-        
+
         auto ctrX = getWidth() / 2;
         auto ctrH = getHeight() / 2;
 
         auto nV = std::ceil(getHeight() / vDistance);
         auto nW = std::ceil(getWidth() / hDistance);
-        
+
         juce::Image litSpheres{juce::Image::ARGB, getWidth(), getHeight(), true};
         juce::Image unlitSpheres{juce::Image::ARGB, getWidth(), getHeight(), true};
         juce::Image sphereBackgrounds{juce::Image::ARGB, getWidth(), getHeight(), true};
@@ -67,7 +67,7 @@ struct LatticeComponent : juce::Component
                 if (y < 0 || y > getHeight())
                     continue;
 
-                for (int w = -nW-1; w < nW + 1; ++w)
+                for (int w = -nW - 1; w < nW + 1; ++w)
                 {
                     float x = w * hDistance + ctrX + off;
 
@@ -86,17 +86,20 @@ struct LatticeComponent : juce::Component
                     }
                     if (sphereLit)
                     {
-                        std::pair<int, int> H = {w + 1, v}; // next one over
-                        std::pair<int, int> U = {w, v + 1}; // next one up
+                        std::pair<int, int> H = {w + 1, v};     // next one over
+                        std::pair<int, int> U = {w, v + 1};     // next one up
                         std::pair<int, int> D = {w + 1, v - 1}; // next one down
                         for (int i = 0; i < 12; ++i)
                         {
-                            if (H == CoO[i]) horizLit = true;
-                            if (U == CoO[i]) upLit = true;
-                            if (D == CoO[i]) downLit = true;
+                            if (H == CoO[i])
+                                horizLit = true;
+                            if (U == CoO[i])
+                                upLit = true;
+                            if (D == CoO[i])
+                                downLit = true;
                         }
                     }
-                    
+
                     // Horizontal Line
                     if (horizLit)
                     {
@@ -110,7 +113,7 @@ struct LatticeComponent : juce::Component
                         juce::Line<float> horiz(x, y, x + hDistance, y);
                         uL.drawLine(horiz, 3.f);
                     }
-                    
+
                     // Upward Line
                     if (upLit)
                     {
@@ -142,55 +145,55 @@ struct LatticeComponent : juce::Component
                         float l[2] = {2.f, 3.f};
                         uL.drawDashedLine(down, l, 2, 3.f, 1);
                     }
-                    
+
                     // Sphere Backgrounds
                     auto ellipseRadius = JIRadius * 1.15;
-                    
+
                     auto gradient = juce::ColourGradient{};
-                     // Select gradient colour
+                    // Select gradient colour
                     if ((w + (v * 4)) % 12 == 0)
                     {
-                        gradient = juce::ColourGradient(com1, x - ellipseRadius, y,
-                                                        com2, x + ellipseRadius, y, false);
+                        gradient = juce::ColourGradient(com1, x - ellipseRadius, y, com2,
+                                                        x + ellipseRadius, y, false);
                     }
                     else if (v == 0)
                     {
-                        gradient = juce::ColourGradient(p1, x - ellipseRadius, y,
-                                                        p2, x + ellipseRadius, y, false);
+                        gradient = juce::ColourGradient(p1, x - ellipseRadius, y, p2,
+                                                        x + ellipseRadius, y, false);
                     }
-                    else if (v == 1 || v == - 1)
+                    else if (v == 1 || v == -1)
                     {
-                        gradient = juce::ColourGradient(l1c1, x - ellipseRadius, y,
-                                                        l1c2, x + ellipseRadius, y, false);
+                        gradient = juce::ColourGradient(l1c1, x - ellipseRadius, y, l1c2,
+                                                        x + ellipseRadius, y, false);
                     }
-                    else if (v == 2 || v == - 2)
+                    else if (v == 2 || v == -2)
                     {
-                        gradient = juce::ColourGradient(l2c1, x - ellipseRadius, y,
-                                                        l2c2, x + ellipseRadius, y, false);
+                        gradient = juce::ColourGradient(l2c1, x - ellipseRadius, y, l2c2,
+                                                        x + ellipseRadius, y, false);
                     }
-                    else if (v == 3 || v == - 3)
+                    else if (v == 3 || v == -3)
                     {
-                        gradient = juce::ColourGradient(l3c1, x - ellipseRadius, y,
-                                                        l3c2, x + ellipseRadius, y, false);
+                        gradient = juce::ColourGradient(l3c1, x - ellipseRadius, y, l3c2,
+                                                        x + ellipseRadius, y, false);
                     }
                     else
                     {
-                        gradient = juce::ColourGradient(l4c1, x - ellipseRadius, y,
-                                                        l4c2, x + ellipseRadius, y, false);
+                        gradient = juce::ColourGradient(l4c1, x - ellipseRadius, y, l4c2,
+                                                        x + ellipseRadius, y, false);
                     }
-                    
+
                     // Names or Ratios?
- //                    auto [n,d] = calculateCell(w, v);
- //                    auto s = std::to_string(n) + "/" + std::to_string(d);
-                    std::string s = jim.nameNoteOnLattice(w,v);
+                    //                    auto [n,d] = calculateCell(w, v);
+                    //                    auto s = std::to_string(n) + "/" + std::to_string(d);
+                    std::string s = jim.nameNoteOnLattice(w, v);
                     // Spheres
                     juce::Path e{};
                     e.addEllipse(x - ellipseRadius, y - JIRadius, 2 * ellipseRadius, 2 * JIRadius);
                     // And their shadows
                     juce::Path b{};
-                    b.addEllipse(x - ellipseRadius - 1.5, y - JIRadius - 1.5, 2 * ellipseRadius + 3, 2 * JIRadius + 3);
-                    
-                    
+                    b.addEllipse(x - ellipseRadius - 1.5, y - JIRadius - 1.5, 2 * ellipseRadius + 3,
+                                 2 * JIRadius + 3);
+
                     if (sphereLit)
                     {
                         whiteShadow.render(lS, e);
@@ -201,9 +204,12 @@ struct LatticeComponent : juce::Component
                         lS.setGradientFill(gradient);
                         lS.fillPath(e);
                         lS.setColour(juce::Colours::white);
-                        lS.drawEllipse(x - ellipseRadius,y - JIRadius, 2 * ellipseRadius, 2 * JIRadius, 3);
+                        lS.drawEllipse(x - ellipseRadius, y - JIRadius, 2 * ellipseRadius,
+                                       2 * JIRadius, 3);
                         lS.setFont(stoke);
-                        lS.drawFittedText(s, x - ellipseRadius + 3, y - (JIRadius / 3.f) , 2.f * (ellipseRadius - 3), .66667f * JIRadius, juce::Justification::horizontallyCentred, 1, 0.05f);
+                        lS.drawFittedText(s, x - ellipseRadius + 3, y - (JIRadius / 3.f),
+                                          2.f * (ellipseRadius - 3), .66667f * JIRadius,
+                                          juce::Justification::horizontallyCentred, 1, 0.05f);
                     }
                     else
                     {
@@ -215,11 +221,14 @@ struct LatticeComponent : juce::Component
                         uS.setGradientFill(gradient);
                         uS.fillPath(e);
                         uS.setColour(juce::Colours::white.withAlpha(.75f));
-                        uS.drawEllipse(x - ellipseRadius,y - JIRadius, 2 * ellipseRadius, 2 * JIRadius, 3);
-//                        auto pst = juce::PathStrokeType(3.f);
-//                        uS.strokePath(e, pst);
+                        uS.drawEllipse(x - ellipseRadius, y - JIRadius, 2 * ellipseRadius,
+                                       2 * JIRadius, 3);
+                        //                        auto pst = juce::PathStrokeType(3.f);
+                        //                        uS.strokePath(e, pst);
                         uS.setFont(stoke);
-                        uS.drawFittedText(s, x - ellipseRadius + 3, y - (JIRadius / 3.f) , 2.f * (ellipseRadius - 3), .66667f * JIRadius, juce::Justification::horizontallyCentred, 1, 0.05f);
+                        uS.drawFittedText(s, x - ellipseRadius + 3, y - (JIRadius / 3.f),
+                                          2.f * (ellipseRadius - 3), .66667f * JIRadius,
+                                          juce::Justification::horizontallyCentred, 1, 0.05f);
                     }
                 }
             }
@@ -234,89 +243,78 @@ struct LatticeComponent : juce::Component
     {
         uint64_t n{1}, d{1};
 
-        while(thirds > 0)
+        while (thirds > 0)
         {
-            auto [nn,dd] = jim.multiplyRatio(n, d, 5, 4);
+            auto [nn, dd] = jim.multiplyRatio(n, d, 5, 4);
             n = nn;
             d = dd;
             thirds--;
         }
 
-        while(thirds < 0)
+        while (thirds < 0)
         {
-            auto [nn,dd] = jim.divideRatio(n, d, 5, 4);
+            auto [nn, dd] = jim.divideRatio(n, d, 5, 4);
             n = nn;
             d = dd;
             thirds++;
         }
 
-        while(fifths > 0)
+        while (fifths > 0)
         {
-            auto [nn,dd] = jim.multiplyRatio(n, d, 3, 2);
+            auto [nn, dd] = jim.multiplyRatio(n, d, 3, 2);
             n = nn;
             d = dd;
             fifths--;
         }
 
-        while(fifths < 0)
+        while (fifths < 0)
         {
-            auto [nn,dd] = jim.divideRatio(n, d, 3, 2);
+            auto [nn, dd] = jim.divideRatio(n, d, 3, 2);
             n = nn;
             d = dd;
             fifths++;
         }
-        
-        auto g = std::gcd(n,d);
+
+        auto g = std::gcd(n, d);
         n = n / g;
         d = d / g;
 
-        return {n,d};
+        return {n, d};
     }
-protected:
+
+  protected:
     static constexpr int JIRadius{26};
     JIMath jim;
-    
-    juce::ReferenceCountedObjectPtr<juce::Typeface> Stoke{ juce::Typeface::createSystemTypefaceFor(LatticesBinary::Stoke_otf, LatticesBinary::Stoke_otfSize)};
-    
+
+    juce::ReferenceCountedObjectPtr<juce::Typeface> Stoke{juce::Typeface::createSystemTypefaceFor(
+        LatticesBinary::Stoke_otf, LatticesBinary::Stoke_otfSize)};
+
     juce::Font stoke{juce::FontOptions(Stoke).withPointHeight(JIRadius)};
 
     juce::Colour com1{0.f, .84f, 1.f, 1.f};
     juce::Colour com2{.961111f, .79f, .41f, .25f};
-    
+
     juce::Colour p1{.5f, .51f, .3f, 1.f};
     juce::Colour p2{.5277778f, .79f, .41f, .25f};
-    
+
     juce::Colour l1c1{.35f, .75f, .98f, 1.f};
     juce::Colour l1c2{.5277778f, .79f, .41f, .25f};
-    
+
     juce::Colour l2c1{.2888889f, .97f, .67f, 1.f};
     juce::Colour l2c2{.6194444f, .71f, 1.f, .25f};
-    
+
     juce::Colour l3c1{.6916667f, .97f, .76f, 1.f};
     juce::Colour l3c2{.4361111f, 1.f, .59f, .61f};
-    
+
     juce::Colour l4c1{.5777778f, .97f, .94f, 58.f};
     juce::Colour l4c2{.8666667f, 1.f, .36f, 1.f};
-    
+
     melatonin::CachedBlur blur{3};
     melatonin::DropShadow blackShadow = {juce::Colours::black, 8};
     melatonin::DropShadow whiteShadow = {juce::Colours::antiquewhite, 12};
-    
-    std::pair<int, int> CoO[12]
-    {
-        {0,0},
-        {0,0},
-        {0,0},
-        {0,0},
-        {0,0},
-        {0,0},
-        {0,0},
-        {0,0},
-        {0,0},
-        {0,0},
-        {0,0},
-        {0,0},
+
+    std::pair<int, int> CoO[12]{
+        {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0},
+        {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0},
     }; // current co-ordinates
 };
-
-
--- src/LatticesEditor.cpp
+++ src/LatticesEditor.cpp
@@ -1,11 +1,11 @@
 /*
   Lattices - A Just-Intonation graphical MTS-ESP Source
-  
+
   Copyright 2023-2024 Andreya Ek Frisk and Paul Walker.
-  
+
   This code is released under the MIT licence, but do note that it depends
   on the JUCE library, see licence for more details.
-  
+
   Source available at https://github.com/Andreya-Autumn/lattices
 */
 
@@ -13,16 +13,15 @@
 #include "LatticesEditor.h"
 
 //==============================================================================
-LatticesEditor::LatticesEditor(LatticesProcessor &p)
-    : juce::AudioProcessorEditor(&p), processor(p)
+LatticesEditor::LatticesEditor(LatticesProcessor &p) : juce::AudioProcessorEditor(&p), processor(p)
 {
     latticeComponent = std::make_unique<LatticeComponent>(p.coOrds);
     addAndMakeVisible(*latticeComponent);
     latticeComponent->setBufferedToImage(true);
-    
-    warningComponent = std::make_unique<MTSWarningComponent>(p);
-    addAndMakeVisible(*warningComponent);
- 
+
+    // warningComponent = std::make_unique<MTSWarningComponent>(p);
+    // addAndMakeVisible(*warningComponent);
+
     if (p.registeredMTS)
     {
         init();
@@ -31,8 +30,7 @@ LatticesEditor::LatticesEditor(LatticesProcessor &p)
     {
         startTimer(1, 50);
     }
-    
-    
+
     setSize(width, height);
     setResizable(true, true);
 }
@@ -53,12 +51,12 @@ void LatticesEditor::resized()
 {
     auto b = this->getLocalBounds();
     latticeComponent->setBounds(b);
-    
+
     if (inited)
     {
         midiButton->setBounds(10, b.getBottom() - 40, 120, 30);
         midiComponent->setBounds(10, b.getBottom() - 155 - 30 - 10, 120, 155);
-        
+
         tuningButton->setBounds(b.getRight() - 216 - 10, b.getBottom() - 40, 216, 30);
         modeComponent->setBounds(b.getRight() - 216 - 10, b.getBottom() - 180 - 40, 216, 90);
         originComponent->setBounds(b.getRight() - 216 - 10, b.getBottom() - 95 - 40, 216, 95);
@@ -73,7 +71,7 @@ void LatticesEditor::showMidiMenu()
 {
     bool show = midiButton->getToggleState();
     midiComponent->setVisible(show);
-    
+
     if (show)
     {
         midiButton->setConnectedEdges(4);
@@ -89,7 +87,7 @@ void LatticesEditor::showTuningMenu()
     bool show = tuningButton->getToggleState();
     originComponent->setVisible(show);
     modeComponent->setVisible(show);
-    
+
     if (show)
     {
         tuningButton->setConnectedEdges(4);
@@ -100,7 +98,7 @@ void LatticesEditor::showTuningMenu()
     }
 }
 
-void LatticesEditor::timerCallback(int timerID) 
+void LatticesEditor::timerCallback(int timerID)
 {
     if (timerID == 1)
     {
@@ -111,7 +109,7 @@ void LatticesEditor::timerCallback(int timerID)
             init();
         }
     }
-    
+
     if (timerID == 0)
     {
         if (processor.changed)
@@ -120,29 +118,26 @@ void LatticesEditor::timerCallback(int timerID)
             latticeComponent->repaint();
             processor.changed = false;
         }
-        
+
         if (modeComponent->modeChanged)
         {
             processor.modeSwitch(modeComponent->whichMode());
             modeComponent->modeChanged = false;
         }
-        
+
         if (midiComponent->settingChanged)
         {
-            processor.updateMIDI(midiComponent->data[0],
-                                 midiComponent->data[1],
-                                 midiComponent->data[2],
-                                 midiComponent->data[3],
-                                 midiComponent->data[4],
-                                 midiComponent->midiChannel);
+            processor.updateMIDI(midiComponent->data[0], midiComponent->data[1],
+                                 midiComponent->data[2], midiComponent->data[3],
+                                 midiComponent->data[4], midiComponent->midiChannel);
         }
-        
+
         if (originComponent->freqChanged)
         {
             processor.updateFreq(originComponent->whatFreq);
             originComponent->freqChanged = false;
         }
-        
+
         if (originComponent->rootChanged)
         {
             int r = originComponent->whichNote();
@@ -155,43 +150,40 @@ void LatticesEditor::init()
 {
     midiButton = std::make_unique<juce::TextButton>("MIDI Settings");
     addAndMakeVisible(*midiButton);
-    midiButton->onClick = [this]{ showMidiMenu(); };
+    midiButton->onClick = [this] { showMidiMenu(); };
     midiButton->setClickingTogglesState(true);
     midiButton->setToggleState(false, juce::dontSendNotification);
-    
-    midiComponent = std::make_unique<MIDIMenuComponent>(processor.shiftCCs[0],
-                                                        processor.shiftCCs[1],
-                                                        processor.shiftCCs[2],
-                                                        processor.shiftCCs[3],
-                                                        processor.shiftCCs[4],
-                                                        processor.listenOnChannel);
+
+    midiComponent = std::make_unique<MIDIMenuComponent>(
+        processor.shiftCCs[0], processor.shiftCCs[1], processor.shiftCCs[2], processor.shiftCCs[3],
+        processor.shiftCCs[4], processor.listenOnChannel);
     addAndMakeVisible(*midiComponent);
     midiComponent->setVisible(false);
-    
+
     tuningButton = std::make_unique<juce::TextButton>("Tuning Settings");
     addAndMakeVisible(*tuningButton);
-    tuningButton->onClick = [this]{ showTuningMenu(); };
+    tuningButton->onClick = [this] { showTuningMenu(); };
     tuningButton->setClickingTogglesState(true);
     tuningButton->setToggleState(false, juce::dontSendNotification);
-    
+
     modeComponent = std::make_unique<ModeComponent>(processor.mode);
     addAndMakeVisible(*modeComponent);
     modeComponent->setVisible(false);
-    
-    originComponent = std::make_unique<OriginComponent>(processor.originalRefNote,
-                                                        processor.originalRefFreq);
+
+    originComponent =
+        std::make_unique<OriginComponent>(processor.originalRefNote, processor.originalRefFreq);
     addAndMakeVisible(*originComponent);
     originComponent->setVisible(false);
-    
+
     auto b = this->getLocalBounds();
-    
+
     midiButton->setBounds(10, b.getBottom() - 40, 120, 30);
     midiComponent->setBounds(10, b.getBottom() - 155 - 30 - 10, 120, 155);
-    
+
     tuningButton->setBounds(b.getRight() - 216 - 10, b.getBottom() - 40, 216, 30);
     modeComponent->setBounds(b.getRight() - 216 - 10, b.getBottom() - 180 - 40, 216, 90);
     originComponent->setBounds(b.getRight() - 216 - 10, b.getBottom() - 95 - 40, 216, 95);
-    
+
     startTimer(0, 5);
     inited = true;
 }
